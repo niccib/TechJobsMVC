07ff52f532f228c916c3cabf97401887c1c4f2a1
Nicole Bishop <nicolejayne915@hotmail.com>
2017-11-27 19:29:21 -0600

    Done

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -13,7 +13,7 @@ namespace TechJobs.Controllers
             return View();
         }
 
-        // TODO #1 - Create a Results action method to process 
+        // Create a Results action method to process 
         // search request and display results
 
         public IActionResult Results(string searchType, string searchTerm)
